85b3834bf521d444ea3b1e2076b47d542a0da925
Adrian1031 <146095031+Adrian1031@users.noreply.github.com>
2023-11-21 15:01:29 +0100

    initial commit

---
+++ JS/domasno3.js
@@ -0,0 +1,12 @@
+// Sekoj vtor neparen broj od 1-100
+for (let i = 1; i <= 100; i += 2) {
+    if (i % 4 === 1) {
+      console.log(i);
+    }
+  }
+  
+// // Sekoj neparen broj od 1-50
+for (let i = 1; i <= 50; i += 2) {
+    console.log(i);
+}
+
